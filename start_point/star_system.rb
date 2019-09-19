446cf39ef0ad81517ae34f488257a524a0f1bdaf
Mark Payne <mjpayne8@gmail.com>
2019-09-19 13:59:35 +0100

    added largest, smallest, no moons, with more moon methods to StarSystem

--- start_point/star_system.rb
+++ start_point/star_system.rb
@@ -13,4 +13,25 @@ class StarSystem
   def get_planet_by_name(planet_name_to_find)
     return @planets.find {|planet| planet.name == planet_name_to_find}
   end
+
+  def get_largest_planet()
+    return @planets.max {|planet_1, planet_2| planet_1.diameter <=> planet_2.diameter}
+  end
+
+  def get_smallest_planet()
+    return @planets.min {|planet_1, planet_2| planet_1.diameter <=> planet_2.diameter}
+  end
+
+  def get_planets_with_no_moons()
+    return @planets.find_all {|planet| planet.number_of_moons == 0}
+  end
+
+  def get_planets_with_more_moons(number_of_moons)
+    planets_with_more_moons = @planets.find_all {|planet| planet.number_of_moons > number_of_moons}
+    planet_names = []
+    planets_with_more_moons.each {|planet| planet_names.push(planet.name)}
+    return planet_names
+  end
+
+  
 end
