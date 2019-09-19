c0588c031ee6b5369dfd47278f0b258dad3504b7
Mark Payne <mjpayne8@gmail.com>
2019-09-19 14:17:33 +0100

    added sort_planets_by_increasing_distance to StarSystem

--- start_point/star_system.rb
+++ start_point/star_system.rb
@@ -36,8 +36,17 @@ class StarSystem
   def get_number_of_planets_closer_than(distance_in_million)
     return @planets.find_all {|planet| planet.distance_from_sun < distance_in_million}.length()
   end
-  def get_total_number_of_moons
+
+  def get_total_number_of_moons()
     return @planets.reduce(0) { |sum, planet| sum + planet.number_of_moons }
   end
-  
+
+  def get_planet_names_sorted_by_increasing_distance_from_sun()
+    sorted_planets = @planets.sort {|planet_1, planet_2| planet_1.distance_from_sun <=> planet_2.distance_from_sun}
+    planet_names = []
+    sorted_planets.each {|planet| planet_names.push(planet.name)}
+    return planet_names
+
+  end
+
 end
