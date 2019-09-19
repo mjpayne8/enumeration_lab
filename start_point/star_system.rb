b231d94a97e33fffdec4f75acd8e1c59107a2f25
Mark Payne <mjpayne8@gmail.com>
2019-09-19 13:41:26 +0100

    initial commit

---
+++ start_point/star_system.rb
@@ -0,0 +1,13 @@
+class StarSystem
+  attr_reader :name, :planets
+
+  def initialize(name, planets)
+    @name = name
+    @planets = planets
+  end
+
+  def planet_names()
+    return @planets.map {|planet| planet.name}
+  end
+
+end
